1f04b2c810d8e6c2e06c9b4b5aa83314b5e7df73
seokhyun03 <tjrgus0303@gmail.com>
2023-07-05 10:28:26 +0900

    페이징 기능 추가로 인한 메서드 변경

--- src/main/java/cash/model/CashbookDao.java
+++ src/main/java/cash/model/CashbookDao.java
@@ -11,13 +11,13 @@ import java.util.List;
 import cash.vo.Cashbook;
 
 public class CashbookDao {
-	public List<Cashbook> selectCashbookListByHashtag(String memberId, int targetYear, int targetMonth, String word){
+	public List<Cashbook> selectCashbookListByHashtag(String memberId, int targetYear, int targetMonth, String word, int beginRow, int rowPerPage){
 		List<Cashbook> list = new ArrayList<Cashbook>();
 		
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-		String sql = "SELECT c.cashbook_no cashbookNo, c.category, c.price, c.memo, c.cashbook_date cashbookDate, c.updatedate, c.createdate FROM cashbook c INNER JOIN hashtag h ON c.cashbook_no = h.cashbook_no WHERE member_id = ? AND YEAR(cashbook_date) = ? AND MONTH(cashbook_date) = ? AND h.word = ? ORDER BY c.cashbook_date ASC";
+		String sql = "SELECT c.cashbook_no cashbookNo, c.category, c.price, c.memo, c.cashbook_date cashbookDate, c.updatedate, c.createdate FROM cashbook c INNER JOIN hashtag h ON c.cashbook_no = h.cashbook_no WHERE member_id = ? AND YEAR(cashbook_date) = ? AND MONTH(cashbook_date) = ? AND h.word = ? ORDER BY c.cashbook_date ASC LIMIT ?,?";
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/cash","root","java1234");
@@ -26,6 +26,8 @@ public class CashbookDao {
 	        stmt.setInt(2, targetYear);
 	        stmt.setInt(3, targetMonth);
 	        stmt.setString(4, word);
+	        stmt.setInt(5, beginRow);
+	        stmt.setInt(6, rowPerPage);
 	        rs = stmt.executeQuery();
 	        System.out.println(stmt);
 	        while(rs.next()) {
@@ -52,6 +54,41 @@ public class CashbookDao {
 		}
 		return list;
 	}
+	
+	public int selectCashbookListByHashtagCnt(String memberId, int targetYear, int targetMonth, String word){
+		int cnt = 0;
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT COUNT(*) FROM cashbook c INNER JOIN hashtag h ON c.cashbook_no = h.cashbook_no WHERE member_id = ? AND YEAR(cashbook_date) = ? AND MONTH(cashbook_date) = ? AND h.word = ?";
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/cash","root","java1234");
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, memberId);
+	        stmt.setInt(2, targetYear);
+	        stmt.setInt(3, targetMonth);
+	        stmt.setString(4, word);
+	        rs = stmt.executeQuery();
+	        System.out.println(stmt);
+	        if(rs.next()) {
+	        	cnt = rs.getInt(1);
+	        }
+		} catch (Exception e1) {
+			e1.printStackTrace();	
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}	
+		}
+		return cnt;
+	}
+	
 	// 반환값 : cashbook_no 키 값
 	public int insertCashbook(Cashbook cashbook) {
 		int cashbookNo = 0;
@@ -125,7 +162,7 @@ public class CashbookDao {
 		return list;
 	}
 	
-	public List<Cashbook> selectCashbookListByDate(String memberId, int targetYear, int targetMonth, int targetDate){
+	public List<Cashbook> selectCashbookListByDate(String memberId, int targetYear, int targetMonth, int targetDate, int beginRow, int rowPerPage){
 		List<Cashbook> list = new ArrayList<Cashbook>();
 		
 		Connection conn = null;
@@ -140,6 +177,8 @@ public class CashbookDao {
 	        stmt.setInt(2, targetYear);
 	        stmt.setInt(3, targetMonth);
 	        stmt.setInt(4, targetDate);
+	        stmt.setInt(5, beginRow);
+	        stmt.setInt(6, rowPerPage);
 	        rs = stmt.executeQuery();
 	        System.out.println(stmt);
 	        while(rs.next()) {
@@ -165,5 +204,39 @@ public class CashbookDao {
 			}	
 		}
 		return list;
+	}
+	
+	public int selectCashbookListByDateCnt(String memberId, int targetYear, int targetMonth, int targetDate){
+		int cnt = 0;
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT COUNT(*) FROM cashbook WHERE member_id = ? AND YEAR(cashbook_date) = ? AND MONTH(cashbook_date) = ? AND DAY(cashbook_date) = ?";
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/cash","root","java1234");
+			stmt = conn.prepareStatement(sql);
+		    stmt.setString(1, memberId);
+	        stmt.setInt(2, targetYear);
+	        stmt.setInt(3, targetMonth);
+	        stmt.setInt(4, targetDate);
+	        rs = stmt.executeQuery();
+	        System.out.println(stmt);
+	        if(rs.next()) {
+	        	cnt = rs.getInt(1);
+	        }
+		} catch (Exception e1) {
+			e1.printStackTrace();	
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}	
+		}
+		return cnt;
 	}
 }
