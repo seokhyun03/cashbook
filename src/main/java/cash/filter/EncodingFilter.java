cfba265ecb47221bb3cdfa04699482f0e8e230be
seokhyun03 <tjrgus0303@gmail.com>
2023-07-07 16:40:52 +0900

    모든 서블릿이 호출되기 전에 utf-8로 인코딩하는 필터 작성

---
+++ src/main/java/cash/filter/EncodingFilter.java
@@ -0,0 +1,20 @@
+package cash.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// 모든 서블릿이 호출되기 전 utf-8로 인코딩
+		request.setCharacterEncoding("utf-8");
+		chain.doFilter(request, response);
+	}
+}
